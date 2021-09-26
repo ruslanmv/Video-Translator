c9aa5ef433265d0e401d3ef58c5e18cf85d75c9e
Ruslan Magana Vsevolodovna <51296249+ruslanmv@users.noreply.github.com>
2021-09-26 22:38:05 +0200

    v2
    It is included translations from English ,Italian ,Japanese ,Russian ,Spanish and German video files

--- main.py
+++ main.py
@@ -15,12 +15,28 @@ def video_to_translate(file_obj,initial_language,final_language):
     videoclip.audio.write_audiofile("test.wav",codec='pcm_s16le')
 # initialize the recognizer
     r = sr.Recognizer()
+
+    if initial_language == "English":
+        lang_in='en-US'
+    elif initial_language == "Italian":
+        lang_in='it-IT'
+    elif initial_language == "Spanish":
+        lang_in='es-MX'
+    elif initial_language == "Russian":
+        lang_in='ru-RU'
+    elif initial_language == "German":
+        lang_in='de-DE'
+    elif initial_language == "Japanese":
+        lang_in='ja-JP'
+
+
+
     # open the file
     with sr.AudioFile("test.wav") as source:
         # listen for the data (load audio to memory)
         audio_data = r.record(source)
         # recognize (convert from speech to text)
-        text = r.recognize_google(audio_data, language = "en-US")
+        text = r.recognize_google(audio_data, language = lang_in)
 
     if final_language == "English":
         lang='en'
@@ -56,12 +72,14 @@ def video_to_translate(file_obj,initial_language,final_language):
 
 
 examples = [
-    [os.path.abspath("steve.mp4")],
-    [os.path.abspath("obama.mp4")]
+    [os.path.abspath("English-Steve-Jobs.mp4")],
+    [os.path.abspath("Russian-Putin.mp4")],
+    [os.path.abspath("Italian-Conte.mp4")],
+    [os.path.abspath("Japanese-DragonBall.mp4")]
 ]
 
 
-initial_language = gr.inputs.Dropdown(["English"])
+initial_language = gr.inputs.Dropdown(["English","Italian","Japanese","Russian","Spanish","German"])
 final_language = gr.inputs.Dropdown([ "Russian","Italian","Spanish","German","English"])
 
 
@@ -70,7 +88,7 @@ gr.Interface(fn = video_to_translate,
             outputs = 'video', 
             verbose = True,
             title = 'Video Translator',
-            description = 'A simple application that translate English video files  to  Italian, Spanish, Russian or English . Upload your own file, or click one of the examples to load them. Wait one minute to process.',
+            description = 'A simple application that translate from English,Italian ,Japanese ,Russian ,Spanish and German video files  to  Italian, Spanish, Russian or English . Upload your own file, or click one of the examples to load them. Wait one minute to process.',
              article = 
                         '''<div>
                             <p style="text-align: center"> All you need to do is to upload the mp4 file and hit submit, then wait for compiling. After that click on Play/Pause for listing to the video. The video is saved in a mp4 format.
